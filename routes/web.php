f691740da66e2331379e655e4e108de5141badeb
rama.dita@bukalapak.com <rama.dita@bukalapak.com>
2025-12-01 20:54:04 +0700

    update admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,11 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('frontend.welcome');
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('backend.dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
